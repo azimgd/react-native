83d7b4a3fc4309e6a353f2867959f01b43f5457d
azimgd <me@azimgd.com>
2023-05-12 01:20:28 +0500

    demo implementation of substring measurement calculation on ios

--- packages/react-native/ReactCommon/react/renderer/components/text/ParagraphLayoutManager.cpp
+++ packages/react-native/ReactCommon/react/renderer/components/text/ParagraphLayoutManager.cpp
@@ -60,12 +60,13 @@ TextMeasurement ParagraphLayoutManager::measure(
   }
 }
 
-LinesMeasurements ParagraphLayoutManager::measureLines(
+SegmentedMeasurements ParagraphLayoutManager::measureLines(
     AttributedString const &attributedString,
     ParagraphAttributes const &paragraphAttributes,
-    Size size) const {
+    Size size,
+    std::vector<int> textLayoutConfig) const {
   return textLayoutManager_->measureLines(
-      attributedString, paragraphAttributes, size);
+      attributedString, paragraphAttributes, size, textLayoutConfig);
 }
 
 void ParagraphLayoutManager::setTextLayoutManager(
